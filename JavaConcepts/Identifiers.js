d7d686eaccfb1c03ce95e34fa31a4abb2ac21427
unknown <chepuri316@gmail.com>
2026-02-14 08:38:47 +0530

    move

---
+++ JavaConcepts/Identifiers.js
@@ -0,0 +1,19 @@
+console.log("helleo team");
+
+
+//identifiers 
+/**
+ * Comments - single line and multiline comment
+ * Operator - operands to perform
+ * identifier-it starts with letter,Underscore,Dolarsign is fine,
+ * Name given to the variable is called identifier
+ * Keywords - it is resereved keyword,it is having his own meaning 
+ * Literals -  variable value 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * * */
